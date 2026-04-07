30e52a62434a9f5b417b80011c291f6925730cd4
Itswali <luckywali0986@gmail.com>
2026-04-07 13:47:23 +0500

    database connected with mongoose and next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+// app/page.tsx
 import React from 'react'
+import connectionToDatabase from '@/lib/db'
 
-export default function page() {
+export default async function Page() {
+  // Call the connection inside the Server Component
+  await connectionToDatabase();
+
   return (
     <div>
-      <h1 className="font-bold">WElcome to DevFLow</h1>
-
+      <h1 className="font-bold">Welcome to DevFlow</h1>
     </div>
   )
 }
---
+++ src/lib/db.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGO_URL;
+
+if (!MONGODB_URI) {
+  throw new Error(process.env.MONGO_URL);
+}
+
+// Check if we have a cached connection to prevent multiple connections during HMR
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+const connectionToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, {
+      dbName: 'devflow', // Good practice to specify DB name
+      bufferCommands: false,
+    }).then((mongoose) => {
+      console.log("Connected to DB");
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+export default connectionToDatabase;
