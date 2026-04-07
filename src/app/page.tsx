fdc70e939f4d7b838eb7fdfb513ca2e0447a6c19
Itswali <luckywali0986@gmail.com>
2026-04-07 14:44:16 +0500

    configured better-auth with mongodb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
-// app/page.tsx
-import React from 'react'
-import connectionToDatabase from '@/lib/db'
+import React from "react";
+import connectionToDatabase from "@/lib/db";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
 export default async function Page() {
-  // Call the connection inside the Server Component
   await connectionToDatabase();
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
 
   return (
     <div>
       <h1 className="font-bold">Welcome to DevFlow</h1>
+      {session ? (
+        <p>Logged in as: {session.user.email}</p>
+      ) : (
+        <p>Not logged in</p>
+      )}
     </div>
-  )
+  );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,14 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import mongoose from "mongoose";
+import connectionToDatabase from "@/lib/db";
+
+await connectionToDatabase();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(mongoose.connection.db),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+});
